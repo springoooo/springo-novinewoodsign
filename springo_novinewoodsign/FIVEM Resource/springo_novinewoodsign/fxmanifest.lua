5782022d8cbd0ef6c007a67d9df2d30bf3662afc
Springo_dev <171420535+springoooo@users.noreply.github.com>
2024-11-10 01:01:50 +1100

    Commit files

---
+++ springo_novinewoodsign/FIVEM Resource/springo_novinewoodsign/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+this_is_a_map 'yes'
+games {'gta5'}
+lua54 'yes'
+
+author 'Springo'
+title 'springo_novinewoodsign'
+description 'Springo | springo_1 | No Vinewood Sign (LODs + Mesh Removed)'
+version '1.0.0'
